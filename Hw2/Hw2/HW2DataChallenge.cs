2792a1d104998877d008a7c0b5a8f5c295cbb427
Smruti Tatavarthy <statavarthy@luc.edu>
2015-06-14 15:06:53 -0500

    Added maintaineance history, Summary and comments

--- Hw2/Hw2/HW2DataChallenge.cs
+++ Hw2/Hw2/HW2DataChallenge.cs
@@ -1,4 +1,37 @@
-﻿using System;
+﻿///////////////////////////////////////////////////////////////////////
+// HW2DataChallenge.cs - Program for HW2 Data Challenge              //
+// Language:    C#, .Net Framework 4.0                               //
+// Application: Open Source Computing, HW#2, Summer 2015             //
+// Author:      SMRUTI TATAVARTHY, COMP 412, Loyola University       //
+//              statavarthy@luc.edu                                  //
+///////////////////////////////////////////////////////////////////////
+/*Summary
+ * 
+ * Analysing the grocery stores in Chicago to see if they have undergone food inspections.
+ * Populating the list of grocery stores to see the ones that have failed the inspection.
+ * Populating the list of grocery stores to see the ones that have not undergone any food inspection.
+ * Verifying if the same grocery stores also have building violations by populating the building violation data.
+ * All the data is considered for the year 2013.
+ * 
+ * Maintenance History:
+ * --------------------
+ * ver 1.4 : 14 Jun 2015
+ * - Added maintaineance history, Summary and comments 
+ *  
+ * ver 1.3 : 14 Jun 2015
+ * - Added the testMode boolean to take partial data for test/debug purposes 
+ * 
+ * ver 1.2 : 14 Jun 2015
+ * - Modified the correlation logic between the Grocery Stores and Food Inspection
+ * 
+ * ver 1.1 : 14 Jun 2015
+ * - Modified the hardcoded path and used relative path
+ * 
+ * ver 1.0 : 13 Jun 2015
+ * - Initial version
+ */
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -41,7 +74,7 @@ namespace Hw2
         public DateTime date;
     }
 
-    class Program
+    class HW2DataChallenge
     {
         static void Main(string[] args)
         {
@@ -52,37 +85,48 @@ namespace Hw2
             TempRecordMatch[] record_match = new TempRecordMatch[10];
 
             int i = 0;
-            int gro_num = 0;
-            long food_num = 0;
+            int groceryStoreCnt = 0;
+            long foodInspectionCnt = 0;
             string dataFolderName = "";
-
+            
+            // boolean to identify the data folder whether test or actual
+            // testMode = true --> take the data from testData folder
+            // testMode = false --> take the data from actualData folder
             bool testMode = false;
 
+            // specify the folder from which data is to be taken
             if (testMode)
                 dataFolderName = "testData";
             else
                 dataFolderName = "actualData";
 
+            // build the full path of the data files
             string filePath = "..\\..\\data\\" + dataFolderName + "\\";
 
+            // STEP - 1 : Parse the "Grocery Stores" data
             var reader = new StreamReader(File.OpenRead(@filePath + "Grocery_Stores_2013.csv"));
             var line0 = reader.ReadLine();
             Console.WriteLine("\n Store Name  License ID \n ");
+
+            // Populating groceryData
             while (!reader.EndOfStream)
             {
                 var line = reader.ReadLine();
                 var values = line.Split(',');
                 groceryData[i].storeName = values[0];
                 groceryData[i].licenseID = values[1];
-                Console.WriteLine("\n {0} {1} ", groceryData[i].storeName, groceryData[i].licenseID);
+                if (testMode)
+                    Console.WriteLine("\n {0} {1} ", groceryData[i].storeName, groceryData[i].licenseID);
                 i++;
             }
-            gro_num = i;
+            groceryStoreCnt = i;
 
             Console.WriteLine("********************************************************************");
             
             long j = 0;
             long num = 0;
+
+            // STEP - 2 : Parse the Food Inspections data
             var reader1 = new StreamReader(File.OpenRead(@filePath + "Food_Inspections_2014.csv"));
             var line0_new = reader1.ReadLine();
             Console.WriteLine("\n License ID  Status \n");
@@ -91,51 +135,52 @@ namespace Hw2
             {
                 var line = reader1.ReadLine();
                 var values = line.Split(',');
-
-                bool chk = long.TryParse(values[0], out num);
-                
+                //checking the start of each line
+                bool chk = long.TryParse(values[0], out num); 
+              
                 if (chk && num > 9999)
                 {
-                    string type = values[4].ToUpper();
-                    string name = "GROCERY";
-                    bool flag = type.Contains(name);
-                    string date = values[10];
-                    bool timeFlag = date.Contains("2013");
+                    string facilityType = values[4].ToUpper();
+                    bool isGroceryStore = facilityType.Contains("GROCERY");
+                    string inspectionDate = values[10];
+                    //Considering records for 2013 only
+                    bool is2013Record = inspectionDate.Contains("2013");
 
-                    if (flag && timeFlag)
+                    // Populating foodInspection data
+                    if (isGroceryStore && is2013Record)
                     {
-
                         foodInspectionData[j].storeLicenseID = values[3];
                         foodInspectionData[j].foodInspectionStatus = values[12];
                         foodInspectionData[j].inspectionDate = values[10];
-                        Console.WriteLine("\n {0}  {1} {2} ", foodInspectionData[j].storeLicenseID, foodInspectionData[j].foodInspectionStatus, foodInspectionData[j].inspectionDate);
+                        if (testMode)
+                            Console.WriteLine("\n {0}  {1} {2} ", foodInspectionData[j].storeLicenseID, foodInspectionData[j].foodInspectionStatus, foodInspectionData[j].inspectionDate);
                         j++;
                     }
 
                 }
 
             }
-            food_num = j;
+            foodInspectionCnt = j;
 
             long x = 0;
             long bldg_num = 0;
             long num1 = 0;
+
+            // STEP - 3 : Parse the Building Violations data
             var reader3 = new StreamReader(File.OpenRead(@filePath + "Building_Violations.csv"));
 
             var line_first = reader3.ReadLine();
             Console.WriteLine("\n Inspection Status  Address \n ");
-                        
+            // Populating foodInspection data           
             while (!reader3.EndOfStream)
             {
-
                 var line = reader3.ReadLine();
                 var values = line.Split(new string[] { "#@" }, StringSplitOptions.None);
-                bool linechk = long.TryParse(values[0], out num1);
-
                 buildingData[x].buildingID = values[0];
                 buildingData[x].buildingAddress = values[16];
-
-                //Console.WriteLine("\n {0}  {1} ", bldg[x].ID, bldg[x].Address);
+                
+                if (testMode)
+                    Console.WriteLine("\n {0}  {1} ", buildingData[x].buildingID, buildingData[x].buildingAddress);
 
                 x++;
             }
@@ -143,13 +188,14 @@ namespace Hw2
 
             int n = 0;
             int c = 0;
-            long m = 0;
+            long m = 0;            
             DateTime maxDate = DateTime.MinValue;
 
-            for (long k = 0; k < gro_num; k++)
+            // Analysis between Grocery Stores and Food Inspection
+            for (long k = 0; k < groceryStoreCnt; k++)
             {
                 bool isInspcted = false;
-                for (m = 0; m < food_num; m++)
+                for (m = 0; m < foodInspectionCnt; m++)
                 {
                     isInspcted = true;
                     if (string.Compare(groceryData[k].licenseID, foodInspectionData[m].storeLicenseID) == 0)
@@ -161,15 +207,19 @@ namespace Hw2
                     c++;
                 }
                 
+                // Considering the latest (max) inspection date for all the matched records
                 foreach (var r in record_match)
                 {
                     if (maxDate < r.date)
                     {
                         maxDate = r.date;
                     }
-                    //Console.WriteLine("\n MAX DATE IS === {0} ", maxDate);
+                    
+                    if (testMode)
+                        Console.WriteLine("\n MAX DATE IS === {0} ", maxDate);
                 }
 
+                // considering only failed inpsection records
                 for (int q = 0; q < record_match.Length; q++)
                 {
 
@@ -187,6 +237,7 @@ namespace Hw2
                     }
                 }
 
+                // populating the grocery stores that are not matched (not been inspected)
                 if (!isInspcted)
                 {
                     finalAnalysis[n].storeInspectionStatus = "NOT INSPECTED";
@@ -196,6 +247,7 @@ namespace Hw2
                 }
             }
 
+            // printing the final result of correlation
             Console.Write("\n analysis data");
 
             for (i = 0; i < n; i++)
