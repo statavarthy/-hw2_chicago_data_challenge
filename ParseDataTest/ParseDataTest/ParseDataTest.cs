c48e743b67f3887bca153edf06aec695be58b443
Smruti Tatavarthy <statavarthy@luc.edu>
2015-06-17 12:41:48 -0500

    Added Unit Test Case for veryfing a case that is not grocery store and not inspected in 2013

--- ParseDataTest/ParseDataTest/ParseDataTest.cs
+++ ParseDataTest/ParseDataTest/ParseDataTest.cs
@@ -77,7 +77,7 @@ namespace Hw2
             catch (Exception e)
             {
                 //Console.WriteLine("Problem in parsing data {0} ", e.StackTrace);
-                Assert.Fail("Problem in parsing {0}", e.StackTrace);
+                Assert.Fail("The parsed data is not a grocery store");
             }
 
         }
@@ -101,7 +101,33 @@ namespace Hw2
             catch (Exception e)
             {
                 //Console.WriteLine("Problem in parsing data {0} ", e.StackTrace);
-                Assert.Fail("Problem in parsing {0}", e.StackTrace);
+                Assert.Fail("The Grocery Store was not inspected in 2013");
+            }
+
+        }
+
+        [Test]
+
+        public void TestForNotGroceryNot2013()
+        {
+            try
+            {
+                string filePath = @"C:\Users\Smruti\Documents\SMRUTI\STUDIES\Loyola\Open Source Computing\Hw2\Hw2_chicago_data_challenge\Hw2\Hw2\data\testData\Food_Inspections_Test_NotGrocery_Not2013.csv";
+                ParseData pd = new ParseData();
+                Hw2.ParseData.FoodInspection[] foodInspectionData = pd.ParseFoodInspection(filePath);
+                Assert.AreEqual(foodInspectionData[0].storeLicenseID, "2391097");
+
+
+            }
+            catch (FileNotFoundException ex)
+            {
+                Assert.Fail("File Not Available");
+                // Console.WriteLine("File Not available {0} ", ex.StackTrace);
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine("Problem in parsing data {0} ", e.StackTrace);
+                Assert.Fail("The parsed data is not a Grocery Store and was not inspected in the year 2013");
             }
 
         }
