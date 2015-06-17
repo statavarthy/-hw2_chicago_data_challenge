bb5d7364ad09aec113b16be8e79890905b77d723
Smruti Tatavarthy <statavarthy@luc.edu>
2015-06-17 18:52:19 -0500

    Merging the test Branch

--- ParseDataTest/ParseDataTest/ParseDataTest.cs
+++ ParseDataTest/ParseDataTest/ParseDataTest.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿///////////////////////////////////////////////////////////////////////////
+// ParsedataTest.cs - Program for Testing methods in the ParseData class //
+// Language:    C#, .Net Framework 4.0                                   //
+// Application: Open Source Computing, HW#2, Summer 2015                 //
+// Author:      SMRUTI TATAVARTHY, COMP 412, Loyola University           //
+//              statavarthy@luc.edu                                      //
+///////////////////////////////////////////////////////////////////////////
+/*Summary
+ * The test files for this Program are under the testData folder
+ * The test cases below are used for checking the functionality of every module in Parse Data
+ * Nunit is required to run the tests below. To run any test it is necessary to build this program first
+ * A reference to the Nunit famework is essential to build the code.
+ * Some of the test cases have been failed deliberately to show that the code is working correctly
+ * All the cases have been considered for the year 2013 and only grocery stores have been taken into account.
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
@@ -8,6 +23,8 @@ using NUnit.Framework;
 
 namespace Hw2
 {
+
+    // Unit Test 1 : To check if the file in the given path is empty. If it is empty the test is failed and message is displayed.
     [TestFixture]
     class ParseDataTest
     {
@@ -17,8 +34,12 @@ namespace Hw2
             string filepath_new= @"C:\Users\Smruti\Documents\SMRUTI\STUDIES\Loyola\Open Source Computing\Hw2\Hw2_chicago_data_challenge\Hw2\Hw2\data\testData\Grocery_Stores_FileChk.csv";
             ParseData pd = new ParseData();
             Hw2.ParseData.Grocery[] groceryData = pd.ParseGrocery(filepath_new);
-            Assert.IsNullOrEmpty(groceryData[0].licenseID, "File Not Empty");
+            Assert.IsNotNull(groceryData[0].licenseID, "File is Empty");
         }
+
+        // Unit Test 2 : To check if the grocery file in the given path is getting parsed. 
+        // Test passes if the condition in assert matches, else the test fails. The license ID in the given file is 980.
+        // Hence this test will pass.
 
         [Test]
         public void ParseGroceryTest()
@@ -42,6 +63,10 @@ namespace Hw2
             }
 
         }
+
+        // Unit Test 3 : To check if the FoodInpection file in the given path is getting parsed. 
+        // Test passes if the condition in assert matches, else the test fails. The license ID in the given file is 2391097
+        // Hence this test will pass.
         [Test]
         public void ParseFoodInspectionTest()
         {
@@ -66,6 +91,10 @@ namespace Hw2
             }
 
         }
+
+        // Unit Test 4 : To check if the data in food inspection is for grocery stores and for the year 2013 
+        // Test passes if the condition in assert matches, else the test fails. The data in the file is not for grocery store
+        // Hence this test will fail.
         [Test]
         public void TestForNotGrocery2013()
         {
@@ -90,6 +119,10 @@ namespace Hw2
             }
 
         }
+
+        // Unit Test 5 : To check if the data in food inspection is for grocery stores and for the year 2013 
+        // Test passes if the condition in assert matches, else the test fails. The data in the file is not for the year 2013
+        // Hence this test will fail.
         [Test]
         public void TestForGroceryNot2013()
         {
@@ -114,6 +147,10 @@ namespace Hw2
             }
 
         }
+
+        // Unit Test 6 : To check if the data in food inspection is for grocery stores and for the year 2013 
+        // Test passes if the condition in assert matches, else the test fails. The data in the file is not for the year 2013 and it is not grocery store
+        // Hence this test will fail.
 
         [Test]
 
@@ -141,6 +178,10 @@ namespace Hw2
 
         }
 
+
+        // Unit Test 7 : To check if the builidng violations file in the given path is getting parsed. 
+        // Test passes if the condition in assert matches, else the test fails. The address in the given file is same as that in the assert condition
+        // Hence this test will pass.
       
         
         [Test]
@@ -169,6 +210,10 @@ namespace Hw2
 
 
         }
+
+        // Unit Test 8 : To check if the data in grocery store is correlated with data in food inspection and building violations 
+        // Test passes if the condition in assert matches, else the test fails. The data in the final analysis has a license id of 980
+        // The correlation of the store with license ID 980 is done with food and building data. Hence this test will pass.
 
 
         [Test]
@@ -205,6 +250,10 @@ namespace Hw2
 
         }
 
+        // Unit Test 9 : To check if the data in grocery store is correlated with data in food inspection and building violations 
+        // This test will check for multiple entries having different inspection dates and will consider the latest record.
+        // The correlation of the store with latest inspection is done with food and building data. Hence this test will pass.
+
         [Test]
         public void AnalysisTestcase2()
         {
@@ -238,6 +287,10 @@ namespace Hw2
 
 
         }
+
+        // Unit Test 10 : To check if the data in grocery store is correlated with data in food inspection and building violations 
+        // This test will check that the final database does not have any records of the stores that have passed the food inpsection.
+        // Hence they will not be populated in the final database. This test will pass since the data is NULL.
         [Test]
         public void AnalysisTestcase3()
         {
